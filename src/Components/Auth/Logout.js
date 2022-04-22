e857f2b294c7a99b5aa038ec26fae2d6889419f9
Ashish M Bhat <61205415+Ashish-M-Bhat@users.noreply.github.com>
2022-04-23 00:27:16 +0530

    Fixed the bug of products not Sorting on searched Products (#13)

--- src/Components/Auth/Logout.js
+++ src/Components/Auth/Logout.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import { useDispatch } from 'react-redux'
 import { authActions } from '../../Store/AuthStore';
 import { cartActions } from '../../Store/CartStore';
