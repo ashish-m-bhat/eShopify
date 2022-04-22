ea19c0dcca1f7b94bceb2893d7b64e922e152280
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2022-04-22 22:10:00 +0530

    Added a loading spinner

--- src/Components/Products/LoadAllProductsFirstTime.js
+++ src/Components/Products/LoadAllProductsFirstTime.js
@@ -1,17 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { fetchProducts } from '../../Store/ProductsStore';
+import LoadingSpinner from '../UI/LoadingSpinner';
 
 export default function LoadAllProductsFirstTime() {
   console.log("Load Products for first time");
     const dispatcher = useDispatch();
+    const [isLoading, setIsLoading] = useState(true); // Set Loading to true immedietly
 
     // indirectly call setAllProductsArray using the thunk fetchProducts()
     useEffect(() => {
       dispatcher(fetchProducts());
+      // Stop the Spinner
+      setIsLoading(false);
     }, [dispatcher])
 
   return (
-    <></>
+    <>
+      {isLoading && <LoadingSpinner />}
+    </>
   )
 }
