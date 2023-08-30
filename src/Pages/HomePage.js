61b29bdc5e3b1f3645f3ba7199007d52bf2d0dcf
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2023-08-30 11:45:51 +0530

    Add files via upload (#23)

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import DisplayProducts from '../Components/Products/DisplayProducts';
+import { useAppSelector } from '../Store/hooks';
 
 // Products to be shown in the homepage. Shows products with rating >=3.5
 
 const MINIMUM_RATING = 3.5;
 export default function HomePage() {
-  const username = useSelector(state => state.auth.username);
+  const username = useAppSelector(state => state.auth.username);
 
-  const allProducts = useSelector(state => state.products.allProductsArray);
+  const allProducts = useAppSelector(state => state.products.allProductsArray);
   const featuredProducts = allProducts.filter(eachProduct => eachProduct.rating.rate >= MINIMUM_RATING)
 
   return (
